afbd1b5dce6bd43e07630197ed896ee60d8a3a37
Valentin Quiedeville <hamilcare@gmail.com>
2017-08-22 00:09:24 +0200

    change speed

--- TarponSimulator2017/Player.cs
+++ TarponSimulator2017/Player.cs
@@ -15,28 +15,41 @@ namespace TarponSimulator2017
 		{
 		}
 
+
+
 		public override void HandleInput(KeyboardState _keyboardState, MouseState mouseState)
 		{
 			
 			// On teste si les touches directionnelles sont enfoncées puis on modifie la position de Zozor en fonction de ça
 			if (_keyboardState.IsKeyDown(Keys.Up))
 			{
-				this._position.Y--;
+				this._position.Y-=_speed;
 
 			}
 			else if (_keyboardState.IsKeyDown(Keys.Down))
 			{
-				this._position.Y++;
+				this._position.Y+=_speed;
 			}
 
 			if (_keyboardState.IsKeyDown(Keys.Right))
 			{
-				this._position.X++;
+				this._position.X+=_speed;
 			}
 			else if (_keyboardState.IsKeyDown(Keys.Left))
 			{
-				this._position.X--;
+				this._position.X-=Speed;
 			}
+
+			if(_keyboardState.IsKeyDown(Keys.A))
+			{
+				this._speed++;
+			}
+
+			if(_keyboardState.IsKeyDown(Keys.Q))
+			{
+				this._speed--;
+			}
+
 
 
 		}
